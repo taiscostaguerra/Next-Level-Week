cc33d5e1168c72da9db71274ee199d3a194f457d
Tais Costa <taiscguerra@gmail.com>
2020-08-06 22:08:58 -0300

    finalizada a aula 4

--- src/server.js
+++ src/server.js
@@ -10,11 +10,11 @@ const proffys = [
     time_to: [1220]
     },
     
-    {name: "Daniela Evangelista",
-    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4",
+    {name: "Mayk Brito",
+    avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
     whatsapp: "899845544",
     bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-    subject: "Química",
+    subject: "Geografia",
     cost:"20",
     weekday: [1],
     time_from: [720],
@@ -23,20 +23,81 @@ const proffys = [
 
 ]
 
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química"
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+]
+
+function getSubject(subjectNumber) {
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
+
+function pageLanding(req,res) {
+    return res.render("index.html")
+}
+
+function pageStudy(req,res) {
+    const filters = req.query
+    //renderizar a pagina recebendo objetos
+    return res.render("study.html", { proffys, filters, subjects, weekdays}) 
+}
+
+function pageGiveClasses(req,res) {
+
+    const data = req.query
+    //transforma o objeto num array
+    const isNotEmpty = Object.keys(data).length 
+    
+    if(isNotEmpty){
+
+        data.subject = getSubject(data.subject)
+
+        //add dados ao objeto proffys
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }
+
+    return res.render("give-classes.html", {subjects, weekdays})
+}
+
 const express = require('express')
 const server = express()
-server.use(express.static("public")) //.use eh config do servidor
 
-.get("/",(req, res) => {
-    return res.sendFile(__dirname + "/views/index.html") //dirname eh o diretorio do server
+//configurar nunjucks (template engine)
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
 })
-.get('/study',(req,res) =>{
-    return res.sendFile(__dirname + "/views/study.html")
-})
-.get('/give-classes',(req,res) =>{
-    return res.sendFile(__dirname + "/views/give-classes.html")
-})
-.get('/',(req,res) =>{
-    return res.sendFile(__dirname + "/views/index.html")
-})
+
+
+//Inicio e configuração do servidor
+server
+//configurar arquivos estaticos
+.use(express.static("public")) //.use eh config do servidor
+//rotas da aplicação
+.get("/",pageLanding)
+.get('/study',pageStudy)
+.get('/give-classes',pageGiveClasses)
+//start do servidor
 .listen(5500)
